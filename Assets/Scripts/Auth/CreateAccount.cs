47cf6a5975190323550061a938a2d8658f3be98a
ems789 <ems789@naver.com>
2024-02-20 18:06:52 +0900

    유효성 검사 작업중

--- Assets/Scripts/Auth/CreateAccount.cs
+++ Assets/Scripts/Auth/CreateAccount.cs
@@ -1,9 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Text.RegularExpressions;
 using TMPro;
 using Unity.VisualScripting;
+using UnityEditor.PackageManager;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using static System.Net.WebRequestMethods;
 
 public class CreateAccount : MonoBehaviour
 {
@@ -15,6 +23,8 @@ public class CreateAccount : MonoBehaviour
     private TMP_InputField inputPassword;
     [SerializeField]
     private TMP_Text textPasswordWarning;
+    [SerializeField]
+    private GameObject notificationUI;
 
     public void CheckInput()
     {
@@ -29,18 +39,19 @@ public class CreateAccount : MonoBehaviour
         else
         {
             textEmailWarning.text = "";
-
         }
 
         if (string.IsNullOrEmpty(inputPassword.text))
         {
             textPasswordWarning.text = "비밀번호를 입력해주세요.";
+            return;
         }
         else
         {
             textPasswordWarning.text = "";
-
         }
+
+        StartCoroutine(Login());
     }
 
     private bool IsVaildEmail(string email)
@@ -48,5 +59,59 @@ public class CreateAccount : MonoBehaviour
         bool valid = Regex.IsMatch(email, @"[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?");
 
         return valid;
+    }
+
+    public enum EErrorCode
+    {
+        None
+    }
+
+    public class ErrorCodeDTO
+    {
+        public EErrorCode Result { get; set; } = EErrorCode.None;
+    }
+
+    [Serializable]
+    public class CreateAccountReq
+    {
+        public string Email;
+        public string Password;
+    }
+
+
+    public class CreateAccountRes : ErrorCodeDTO
+    {
+    }
+
+    IEnumerator Login()
+    {
+        string url = "https://localhost:7034/Account/Create";
+
+        CreateAccountReq request = new CreateAccountReq
+        {
+            Email = inputEmail.text,
+            Password = inputPassword.text
+        };
+
+        string jsonRequest = JsonUtility.ToJson(request);
+        byte[] jsonBytes = Encoding.UTF8.GetBytes(jsonRequest); // JSON 문자열을 바이트 배열로 변환
+
+        UnityWebRequest www = new UnityWebRequest(url, "POST");
+        www.uploadHandler = new UploadHandlerRaw(jsonBytes);
+        www.SetRequestHeader("Content-Type", "application/json");
+
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.LogError("HTTP Error: " + www.error);
+        }
+        else
+        {
+            string jsonResponse = www.downloadHandler.text;
+            CreateAccountRes response = JsonUtility.FromJson<CreateAccountRes>(jsonResponse);
+
+            Debug.Log("Login Response: " + response);
+        }
     }
 }
