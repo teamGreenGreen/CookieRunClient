9883d42022439d4abfd22bcda61a067c46033afe
KongJJoki <jeonga0208@gmail.com>
2024-02-20 23:00:16 +0900

    friendUI

---
+++ Assets/Scripts/UI/OpenCanvas.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenCanvas : MonoBehaviour
+{
+    [SerializeField]
+    private Canvas targetCanvas;
+    private Button openButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnClick()
+    {
+        targetCanvas.gameObject.SetActive(true);
+    }
+}
