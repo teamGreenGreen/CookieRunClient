99a2d776c0190fa8108666d302830ed878beba53
KongJJoki <147111815+KongJJoki@users.noreply.github.com>
2024-02-26 12:20:48 +0900

    friendAcceptBox

--- Assets/Scripts/Friend/FriendDelete.cs
+++ Assets/Scripts/Friend/FriendDelete.cs
@@ -13,6 +13,8 @@ using UnityEngine.UI;
 public class FriendDelete : MonoBehaviour
 {
     [SerializeField]
+    public GameObject ErrorPrefab;
+    [SerializeField]
     private TextMeshProUGUI friendNameTxt;
     public async void OnClick()
     {
@@ -25,7 +27,26 @@ public class FriendDelete : MonoBehaviour
 
         if (res.Result == 0)
         {
-            Debug.Log("삭제 성공");
+            GameObject warningPrefab = Instantiate(ErrorPrefab);
+
+            Transform alertTxtTransform = warningPrefab.transform.Find("Alert_Txt");
+            TextMeshProUGUI alertText = alertTxtTransform.GetComponentInChildren<TextMeshProUGUI>();
+            alertText.text = "친구를 삭제했습니다.";
+
+            Canvas canvas = GetComponentInParent<Canvas>();
+
+            if (canvas != null)
+            {
+                RectTransform prefabRectTransform = warningPrefab.GetComponent<RectTransform>();
+                prefabRectTransform.SetParent(canvas.transform, false);
+                prefabRectTransform.localPosition = Vector3.zero;
+                warningPrefab.transform.SetParent(canvas.transform, false);
+            }
+            else
+            {
+                Debug.LogError("CookieSelect_Canvas 스크립트가 붙은 객체의 상단 부모에 캔버스가 없습니다.");
+            }
+            return;
         }
         else
         {
--- Assets/Scripts/Friend/FriendRequestDeny.cs
+++ Assets/Scripts/Friend/FriendRequestDeny.cs
@@ -12,6 +12,8 @@ using Assets.Scripts.DAO.GameServer;
 public class FriendRequestDeny : MonoBehaviour
 {
     [SerializeField]
+    public GameObject ErrorPrefab;
+    [SerializeField]
     private TextMeshProUGUI requestIdTxt;
     public async void OnClick()
     {
@@ -24,7 +26,26 @@ public class FriendRequestDeny : MonoBehaviour
 
         if (res.Result == 0)
         {
-            Debug.Log("거절 성공");
+            GameObject warningPrefab = Instantiate(ErrorPrefab);
+
+            Transform alertTxtTransform = warningPrefab.transform.Find("Alert_Txt");
+            TextMeshProUGUI alertText = alertTxtTransform.GetComponentInChildren<TextMeshProUGUI>();
+            alertText.text = "요청을 거절했습니다.";
+
+            Canvas canvas = GetComponentInParent<Canvas>();
+
+            if (canvas != null)
+            {
+                RectTransform prefabRectTransform = warningPrefab.GetComponent<RectTransform>();
+                prefabRectTransform.SetParent(canvas.transform, false);
+                prefabRectTransform.localPosition = Vector3.zero;
+                warningPrefab.transform.SetParent(canvas.transform, false);
+            }
+            else
+            {
+                Debug.LogError("CookieSelect_Canvas 스크립트가 붙은 객체의 상단 부모에 캔버스가 없습니다.");
+            }
+            return;
         }
         else
         {
